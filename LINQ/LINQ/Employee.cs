5d902e6c1e3691a0f157951059195a059379db5f
Achini Randeni <achinirandeni@gmail.com>
2024-08-01 13:45:59 +0800

    Initial commit

---
+++ LINQ/LINQ/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+
+    public class EmployeeBand
+    {
+        public virtual Employee Employee { get; set; }
+        public int EmpId { get; set; }
+        public int EmpBand { get; set; }
+    }
+
+    public class EmployeeSalary
+    {
+        public virtual Employee Employee { get; set; }
+        public int EmpId { get; set; }
+        public int EmpSalary { get; set; }
+    }
+
+    public class EmployeeProject
+    {
+        public virtual Employee Employee { get; set; }
+        public virtual Projects Projects { get; set; }
+        public int EmpId { get; set; }
+        public int ProjectId { get; set; }
+    }
+
+    public class Projects
+    {
+        public int ProjectId { get; set; }
+        public string Project { get; set; }
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using LINQ;
+
+IList<Employee> employees = new List<Employee>()
+{
+    new Employee(){ EmpId = 123, Name = "Jon", DOB = new DateTime(1989, 01, 07)},
+    new Employee(){ EmpId = 124, Name = "Alex", DOB= new DateTime(1985, 07, 06)},
+};
+
+IList<EmployeeBand> employeeBands = new List<EmployeeBand>()
+{
+    new EmployeeBand(){ EmpId = 123, EmpBand=1},
+    new EmployeeBand(){ EmpId = 124, EmpBand=2}
+};
+
+IList<EmployeeSalary> employeeSalaries = new List<EmployeeSalary>()
+{
+    new EmployeeSalary(){ EmpId = 123, EmpSalary = 20000 },
+    new EmployeeSalary(){ EmpId = 124, EmpSalary = 80000},
+     new EmployeeSalary(){ EmpId = 125, EmpSalary = 20000},
+};
+
+IList<EmployeeProject> employeeProjects = new List<EmployeeProject>()
+{
+    new EmployeeProject(){ EmpId = 123, ProjectId = 1},
+    new EmployeeProject(){ EmpId = 124, ProjectId = 2},
+    new EmployeeProject(){ EmpId = 125, ProjectId = 2}
+};
+
+IList<Projects> projects = new List<Projects>() {
+    new Projects(){ ProjectId = 1, Project = "ABC"},
+    new Projects(){ ProjectId = 2, Project = "PQS"}
+};
+
+var selectedEmployees = from emp in employees
+                        join bnd in employeeBands on emp.EmpId equals bnd.EmpId
+                        join salary in employeeSalaries on emp.EmpId equals salary.EmpId
+                        where salary.EmpSalary > 50000
+                        select new
+                        {
+                            EmpId = emp.EmpId,
+                            EmpName = emp.Name,
+                            Band = bnd.EmpBand
+                        };
+
+var sumOfSalaries = from salary in employeeSalaries
+                    join empProj in employeeProjects on salary.EmpId equals empProj.EmpId
+                    join proj in projects on empProj.ProjectId equals proj.ProjectId
+                    where proj.Project == "PQS"
+                    select salary;
+
+var sum = sumOfSalaries.Sum(s => s.EmpSalary);
+
+Console.WriteLine("Sum:" + sum);
+
